cf33ccbe71bf28a1c461a2c8232d1b7f631053fd
Sneha796 <snehanikam796@gmail.com>
2026-04-16 10:49:54 +0530

    Create Shopping.py

---
+++ Shopping.py
@@ -0,0 +1,7 @@
+item = input("What item would you like to buy? : ") 
+price = float(input(" What is the price of an item? : ")) 
+quantity = int(input("How many item would you like to buy? :)) 
+total = price * quantity
+
+print("You have bought {quantity} * {item}")
+print (f"Your total is: {total}") 
