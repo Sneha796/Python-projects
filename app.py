15368fe1b1f54ef0151b4bb99b1647b4ad85342b
Sneha Nikam <snehanikam796@gmail.com>
2026-07-02 12:46:20 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-item = input("What item would you like to buy? : ") 
-price = float(input(" What is the price of an item? : ")) 
-quantity = int(input("How many item would you like to buy? :")) 
-total = price * quantity
-
-print(f"You have bought {quantity} * {item}")
-print (f"Your total is: {total}") 
